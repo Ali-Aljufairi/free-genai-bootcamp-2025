a3c6796ac9065c3d024c57ff6ebc70ccb4b86dc4
Ali Redha <ali.redha.ali@gmail.com>
2025-03-16 20:36:51 +0900

    Add study_session_id field to study_activities table and update foreign key constraints

--- lang-portal/internal/database/migrations/0004_create_study_activities_table.sql
+++ lang-portal/internal/database/migrations/0004_create_study_activities_table.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS study_activities (
     description TEXT,
     thumbnail_url TEXT,
     group_id INTEGER,
+    study_session_id INTEGER,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+    FOREIGN KEY (study_session_id) REFERENCES study_sessions(id) ON DELETE CASCADE
 );
