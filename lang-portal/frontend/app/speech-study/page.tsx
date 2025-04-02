ac0c3273cf88f4e6a9f2ad988660a58a502fdade
Ali Redha <ali.redha.ali@gmail.com>
2025-04-03 00:39:14 +0900

    Remove SpeechStudyPage component from speech-study directory

--- lang-portal/frontend/app/speech-study/page.tsx
+++
@@ -1,9 +0,0 @@
-import SpeechStudy from '@/components/study/speech-study';
-
-export default function SpeechStudyPage() {
-    return (
-        <main className="min-h-screen bg-white">
-            <SpeechStudy />
-        </main>
-    );
-} 
